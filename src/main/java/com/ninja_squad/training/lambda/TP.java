b5736bde10f8360465c6f0fcc0999accbf9590e2
JB Nizet <jnizet@gmail.com>
2013-04-23 08:00:59 +0200

    implement solution

--- src/main/java/com/ninja_squad/training/lambda/TP.java
+++ src/main/java/com/ninja_squad/training/lambda/TP.java
@@ -1,8 +1,11 @@
 package com.ninja_squad.training.lambda;
 
+import java.util.Comparators;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Le TP Lambda
@@ -14,92 +17,132 @@ public class TP {
      * Ecrivez les dates des tweets sur la sortie standard
      */
     public static void step1() {
-        // TODO
+        Tweet.TWEETS.forEach(t -> System.out.println(t.getDate()));
     }
 
     /**
      * Faites la même chose, sans appeler getDate() ni System.out.println()
      */
     public static void step2() {
-        // TODO
+        Tweet.TWEETS
+             .stream()
+             .map(Tweet::getDate)
+             .forEach(System.out::println);
     }
 
     /**
      * Extrayez une List<String> qui contient les senders des tweets
      */
     public static List<String> step3() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .map(Tweet::getSender)
+                    .collect(Collectors.toList());
     }
 
     /**
      * Extrayez une List<String> qui contient les senders des tweets, sans duplicata
      */
     public static List<String> step4() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .map(Tweet::getSender)
+                    .distinct()
+                    .collect(Collectors.toList());
     }
 
     /**
      * Extrayez une List<String> qui contient les senders des tweets, sans duplicata, triés par ordre alphabétique
      */
     public static List<String> step5() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .map(Tweet::getSender)
+                    .distinct()
+                    .sorted()
+                    .collect(Collectors.toList());
     }
 
     /**
      * Extrayez une List<Tweet> qui contient les tweets contenant le hashtag #lambda
      */
     public static List<Tweet> step6() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .filter(t -> t.containsHashTag("#lambda"))
+                    .collect(Collectors.toList());
     }
 
     /**
      * Extrayez une List<Tweet> qui contient les tweets contenant le hashtag #lambda, triés par sender puis par date
      */
     public static List<Tweet> step7() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .filter(t -> t.containsHashTag("#lambda"))
+                    .sorted(Comparators.<Tweet, String>comparing((Tweet t) -> t.getSender())
+                                       .thenComparing((Tweet t) -> t.getDate()))
+                    .collect(Collectors.toList());
     }
 
     /**
      * Extrayez un Set<String> qui contient l'ensemble des hash tags des tweets
      */
     public static Set<String> step8() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .flatMap(t -> t.getHashTags().stream())
+                    .collect(Collectors.toSet());
     }
 
     /**
      * Créez une Map<String, List<Tweet>> qui contient, pour chaque sender, les tweets envoyés par ce sender
      */
     public static Map<String, List<Tweet>> step9() {
-        // TODO
-        return null;
+        Map<String, List<Tweet>> tweetsPerUser =
+            Tweet.TWEETS
+                 .stream()
+                 .collect(Collectors.groupingBy(Tweet::getSender, Collectors.toList()));
+        return tweetsPerUser;
     }
 
     /**
      * Extrayez deux listes: les tweets qui contiennent le hash tag #lambda, et ceux qui ne les contiennent pas.
      */
     public static Map<Boolean, List<Tweet>> step10() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .collect(Collectors.partitioningBy(t -> t.containsHashTag("#lambda")));
     }
 
     public static class Stats {
-        // TODO
+
+        private final int tweetCount;
+        private final int characterCount;
+
+        public Stats(int tweetCount, int characterCount) {
+            this.tweetCount = tweetCount;
+            this.characterCount = characterCount;
+        }
+
+        public Stats(Tweet tweet) {
+            this.tweetCount = 1;
+            this.characterCount = tweet.getText().length();
+        }
+
+        public Stats withStats(Stats stats) {
+            return new Stats(this.tweetCount + stats.tweetCount, this.characterCount + stats.characterCount);
+        }
 
         public int getAverage() {
-            // TODO
-            return 0;
+            return tweetCount == 0 ? 0 : (characterCount / tweetCount);
         }
 
         public int getTotal() {
-            // TODO
-            return 0;
+            return characterCount;
         }
+
+
     }
 
     /**
@@ -109,15 +152,25 @@ public class TP {
      *     Utilisez stream.collect(..., ..., ...) ou stream.map(...).reduce(...)
      */
     public static Stats step11() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .stream()
+                    .map(t -> new Stats(t))
+                    .reduce(new Stats(0, 0), (t1, t2) -> t1.withStats(t2));
     }
 
     /**
      * Faites la même chose, mais de manière parrallèle
      */
     public static Stats step12() {
-        // TODO
-        return null;
+        return Tweet.TWEETS
+                    .parallelStream()
+                    .map(t -> new Stats(t))
+                    .reduce(new Stats(0, 0), (t1, t2) -> t1.withStats(t2));
+    }
+
+    public static Map<Long, Tweet> step13() {
+        return Tweet.TWEETS.stream().collect(() -> new HashMap<Long, Tweet>(),
+                                             (map, tweet) -> map.put(tweet.getId(), tweet),
+                                             (map1, map2) -> map1.putAll(map2));
     }
 }
